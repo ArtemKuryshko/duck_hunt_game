917e8c3df7505f2922d0900c187103cdac9417cc
Artem Kuryshko <kuryshkoartem05@gmail.com>
2026-03-22 15:13:04 +0200

    refactor: Normalize ASSETS_DIR path, update BG_PATH in config

--- src/config.py
+++ src/config.py
@@ -1,15 +1,16 @@
 import os
 
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 FPS = 60
-TITLE ="Duck Hunt"
+TITLE = "Duck Hunt"
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-ASSETS_DIR = os.path.join(BASE_DIR, "..", "assets") 
-BG_PATH = os.path.join(ASSETS_DIR, "images","backgrounds", "Background.png")
-TREES_PATH = os.path.join(ASSETS_DIR, "images","backgrounds", "Trees.png")
-GRASS_PATH = os.path.join(ASSETS_DIR, "images","backgrounds", "Grass.png")
-ANIMATIONS_PATH = os.path.join(ASSETS_DIR, "images","models", "birds")
-SCOREBOARD_PATH = os.path.join(ASSETS_DIR, "images","interface", "Scoreboard.png")
+ASSETS_DIR = os.path.normpath(os.path.join(BASE_DIR, "..", "assets"))
+BG_PATH = os.path.join(ASSETS_DIR, "images", "backgrounds", "Background.png")
+TREES_PATH = os.path.join(ASSETS_DIR, "images", "backgrounds", "Trees.png")
+GRASS_PATH = os.path.join(ASSETS_DIR, "images", "backgrounds", "Grass.png")
+ANIMATIONS_PATH = os.path.join(ASSETS_DIR, "images", "models", "birds")
+SCOREBOARD_PATH = os.path.join(ASSETS_DIR, "images", "interface", "Scoreboard.png")
