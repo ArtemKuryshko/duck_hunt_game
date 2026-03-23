e3f83ea68678f1b28e8c26e27ea43d1965ab244a
Artem Kuryshko <kuryshkoartem05@gmail.com>
2026-03-23 19:08:05 +0200

    feat: Modify birds entities properties

--- src/entities/birds/badCrow.py
+++ src/entities/birds/badCrow.py
@@ -1,14 +1,20 @@
 from .base import BaseBird
 from typing import Dict, List
 import pygame
+
 class BadCrow(BaseBird):
     def __init__(self, x: int, y: int, animations: Dict[str, List[pygame.Surface]]):
         super().__init__(x, y, animations)
-
         self.health = 1
-        self.speed = 1
+        self.speed = 1.5
+        self.current_direction = "Up"
         
     def move(self):
-        pass
+        self.y -= self.speed
+        self.rect.topleft = (self.x, self.y)
+
     def animate(self):
-        pass
+        frames = self.animations.get(self.current_direction, [])
+        if frames:
+            self.current_frame = (self.current_frame + 1) % (len(frames) * 10)
+            self.image = frames[self.current_frame // 10]
