9d71c2ee63cb6f5f5a8228f3a9243787a4e6fe8c
llunasv <vladaglov@gmail.com>
2026-05-04 16:46:24 +0300

    fix: starting ammo
    When the game starts starting ammo equals the maximum capacity of the weapon equipped

--- src/core/game.py
+++ src/core/game.py
@@ -76,6 +76,7 @@ class Game:
                 if new_state == GameState.GAME and self.state != GameState.GAME:
                     self.score_system.reset()
                     self.level_manager.birds.clear()
+                    self.current_weapon = WeaponFactory.create_weapon(self.current_weapon_name)
                 self.state = new_state
 
             elif self.state == GameState.SHOP:
