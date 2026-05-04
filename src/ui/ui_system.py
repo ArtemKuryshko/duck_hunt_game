b0dc248ecabf48e597d78f615fc23f4962580c09
llunasv <vladaglov@gmail.com>
2026-05-04 17:13:00 +0300

    feat: ui update
    Updated game ui and added points showing uo in the main menu

--- src/ui/ui_system.py
+++ src/ui/ui_system.py
@@ -1,11 +1,13 @@
 import pygame
+import os
 from config import (
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
     HEART_PATH,
     GREY_HEART_PATH,
     CROSSHAIR_PATH,
-    FONT_PATH
+    FONT_PATH,
+    WEAPONS_PATH
 )
 
 
@@ -23,6 +25,24 @@ class UISystem:
         self.heart_empty = pygame.transform.scale(self.heart_empty, icon_size)
         self.crosshair = pygame.transform.scale(self.crosshair, (50, 50))
 
+        self.weapon_images = {}
+        weapon_files = {
+            "Pistol": "pistol.png",
+            "Revolver": "revolver.png",
+            "Light Rifle": "light rifle.png",
+            "Shotgun": "shotgun.png",
+            "Rifle": "autonatic rifle.png"
+        }
+
+        for w_name, file_name in weapon_files.items():
+            try:
+                img = pygame.image.load(os.path.join(WEAPONS_PATH, file_name)).convert_alpha()
+                ratio = 60 / img.get_width()
+                w_height = int(img.get_height() * ratio)
+                self.weapon_images[w_name] = pygame.transform.smoothscale(img, (60, w_height))
+            except:
+                self.weapon_images[w_name] = pygame.Surface((60, 40))
+
     def draw_health_ui(self, screen: pygame.Surface, health: int):
         for i in range(3):
             x_pos = 375 + (i * 20)
@@ -35,7 +55,7 @@ class UISystem:
     def draw_game_ui(self, screen: pygame.Surface, score: int, health: int, current_weapon):
         # Draw score
         score_text = self.font.render(f"{score}", False, (255, 255, 255))
-        screen.blit(score_text, (240, 540))
+        screen.blit(score_text, (140, 512))
 
         self.draw_health_ui(screen, health)
         if current_weapon.is_reloading:
@@ -43,6 +63,10 @@ class UISystem:
         else:
             ammo_surf = self.small_font.render(f"{current_weapon.current_ammo} / {current_weapon.ammo_capacity}", True, (255, 255, 255))
         screen.blit(ammo_surf, (616, 530))
+
+        if current_weapon.name in self.weapon_images:
+            w_img = self.weapon_images[current_weapon.name]
+            screen.blit(w_img, (550, 510))
 
     def draw_crosshair(self, screen: pygame.Surface):
         pos = pygame.mouse.get_pos()
